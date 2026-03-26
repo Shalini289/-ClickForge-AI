fba5b8e93bfbd881f789b074000526b6de8edecb
Shalini <shalinibhadouriya95@gmail.com>
2026-03-26 13:17:37 +0530

    connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const generateThumbnail = async (topic) => {
-  const res = await axios.post("http://localhost:5000/generate", {
+  const res = await axios.post("https://clickforge-ai.onrender.com/generate", {
     topic
   });
   return res.data;
